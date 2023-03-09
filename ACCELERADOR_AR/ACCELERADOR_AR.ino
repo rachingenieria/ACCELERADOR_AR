a0915dba9b5806d0202558ef73677d67687eec9b
Ricardo Cervantes <rachingenieria.x@gmail.com>
2023-03-08 20:24:58 -0500

    added documents

--- ACCELERADOR_AR/ACCELERADOR_AR.ino
+++ ACCELERADOR_AR/ACCELERADOR_AR.ino
@@ -1,9 +1,20 @@
-void setup() {
-  // put your setup code here, to run once:
+const int Sensor1Pin = 2;     // the number of the pushbutton pin
+const int Sensor2Pin =  12;      // the number of the LED pin
 
+int Sensor1State;
+int Sensor2State;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(Sensor1Pin, INPUT);
+  pinMode(Sensor2Pin, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  
+  Sensor1State = digitalRead(Sensor1Pin);
+  Sensor2State = digitalRead(Sensor2Pin);
+  Serial.print(Sensor1State);
+  Serial.print(',');
+  Serial.println(Sensor2State);
 }
